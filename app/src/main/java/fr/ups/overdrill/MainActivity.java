12e99db0dbe19ed418dc89cf1ecce80a4b9449ce
Jan Jaap de Groot <jjedegroot@gmail.com>
2016-09-28 14:23:58 +0200

    Sensor library attached to app

--- app/src/main/java/fr/ups/overdrill/MainActivity.java
+++ app/src/main/java/fr/ups/overdrill/MainActivity.java
@@ -2,16 +2,30 @@ package fr.ups.overdrill;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import fr.ups.sensoractions.ShakeListener;
 
 /**
  * Main activity
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ShakeListener.OnShakeListener {
+
+    private ShakeListener shakeListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Listen to shake events
+        this.shakeListener = new ShakeListener(this);
+        shakeListener.setOnShakeListener(this);
+    }
+
+    @Override
+    public void onShake() {
+        Toast.makeText(this, "SHAKE", Toast.LENGTH_SHORT).show();
     }
 
 }
