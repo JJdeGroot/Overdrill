9a9dd017b8cd475a45d38ec99dd1771b774d4643
Jan Jaap de Groot <jjedegroot@gmail.com>
2016-09-28 16:19:28 +0200

    Styling of application

--- app/src/main/java/fr/ups/overdrill/MainActivity.java
+++ app/src/main/java/fr/ups/overdrill/MainActivity.java
@@ -1,5 +1,6 @@
 package fr.ups.overdrill;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,12 +8,17 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import fr.ups.overdrill.hiscore.HiscoreActivity;
+import fr.ups.overdrill.info.InfoActivity;
 import fr.ups.sensoractions.ShakeListener;
 
 /**
  * Main activity
  */
 public class MainActivity extends AppCompatActivity implements ShakeListener.OnShakeListener {
+
+    private static final int REQUEST_CODE_INFO = 1,
+                             REQUEST_CODE_HISCORE = 2;
 
     private ShakeListener shakeListener;
 
@@ -31,19 +37,22 @@ public class MainActivity extends AppCompatActivity implements ShakeListener.OnS
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
         if(id == R.id.action_info) {
             // Info
-
+            Intent intent = new Intent(this, InfoActivity.class);
+            startActivityForResult(intent, REQUEST_CODE_INFO);
         }else if(id == R.id.action_sound) {
             // Sound
-
+            // TODO: Change sound setting
         }else if(id == R.id.action_hiscore) {
             // Hisores
+            Intent intent = new Intent(this, HiscoreActivity.class);
+            startActivityForResult(intent, REQUEST_CODE_HISCORE);
         }
 
         return super.onOptionsItemSelected(item);
