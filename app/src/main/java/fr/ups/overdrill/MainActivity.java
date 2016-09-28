0dd3a2e33d5c1fb9b724a0b3a999ab59f71d8780
Jan Jaap de Groot <jjedegroot@gmail.com>
2016-09-28 16:02:45 +0200

    Icons as menu items

--- app/src/main/java/fr/ups/overdrill/MainActivity.java
+++ app/src/main/java/fr/ups/overdrill/MainActivity.java
@@ -2,6 +2,9 @@ package fr.ups.overdrill;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import fr.ups.sensoractions.ShakeListener;
@@ -21,6 +24,29 @@ public class MainActivity extends AppCompatActivity implements ShakeListener.OnS
         // Listen to shake events
         this.shakeListener = new ShakeListener(this);
         shakeListener.setOnShakeListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_info) {
+            // Info
+
+        }else if(id == R.id.action_sound) {
+            // Sound
+
+        }else if(id == R.id.action_hiscore) {
+            // Hisores
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
