fbe5eb3ba133a35a1558908d55fcafb62048de8f
Jan Jaap de Groot <jjedegroot@gmail.com>
2016-09-28 15:32:47 +0200

    Icons for everything

---
+++ app/src/main/java/fr/ups/overdrill/hiscore/HiscoreActivity.java
@@ -0,0 +1,18 @@
+package fr.ups.overdrill.hiscore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.ups.overdrill.R;
+
+/**
+ * Activity where the hisore is shown.
+ */
+public class HiscoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hiscore);
+    }
+}
---
+++ app/src/main/java/fr/ups/overdrill/info/InfoActivity.java
@@ -0,0 +1,18 @@
+package fr.ups.overdrill.info;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.ups.overdrill.R;
+
+/**
+ * Activity where all sensors are listed.
+ */
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
